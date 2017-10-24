492449700e85f723a8b19f5061d7f812a41d0db5
Xin Wang <dram.wang@gmail.com>
2017-10-24 12:32:52 +0800

    Split clips subcommands into separate functions

--- sources/clips_interface.c
+++ sources/clips_interface.c
@@ -6,6 +6,116 @@
 
 #include "interface.h"
 
+static int tcl_clips_AssertString(Tcl_Interp *interp,
+				  Environment *env,
+				  int objc,
+				  Tcl_Obj *const objv[])
+{
+	AssertString(env, Tcl_GetString(objv[2]));
+
+	return TCL_OK;
+}
+
+static int tcl_clips_BatchStar(Tcl_Interp *interp,
+			       Environment *env,
+			       int objc,
+			       Tcl_Obj *const objv[])
+{
+	BatchStar(env, Tcl_GetString(objv[2]));
+
+	return TCL_OK;
+}
+
+static int tcl_clips_Build(Tcl_Interp *interp,
+			   Environment *env,
+			   int objc,
+			   Tcl_Obj *const objv[])
+{
+	Build(env, Tcl_GetString(objv[2]));
+
+	return TCL_OK;
+}
+
+static int tcl_clips_DefglobalGetValue(Tcl_Interp *interp,
+				       Environment *env,
+				       int objc,
+				       Tcl_Obj *const objv[])
+{
+	CLIPSValue v;
+	DefglobalGetValue(
+		*(void **) Tcl_GetByteArrayFromObj(objv[2], NULL), &v);
+	Tcl_SetObjResult(interp, Tcl_NewIntObj(v.integerValue->contents));
+
+	return TCL_OK;
+}
+
+static int tcl_clips_Eval(Tcl_Interp *interp,
+			  Environment *env,
+			  int objc,
+			  Tcl_Obj *const objv[])
+{
+	Eval(env, Tcl_GetString(objv[2]), NULL);
+
+	return TCL_OK;
+}
+
+static int tcl_clips_FindDefglobal(Tcl_Interp *interp,
+				   Environment *env,
+				   int objc,
+				   Tcl_Obj *const objv[])
+{
+	Defglobal *r = FindDefglobal(env, Tcl_GetString(objv[2]));
+
+	if (r == NULL) {
+	} else {
+		Tcl_SetObjResult(interp,
+				 Tcl_NewByteArrayObj((void *) &r, sizeof(r)));
+	}
+
+	return TCL_OK;
+}
+
+static int tcl_clips_Load(Tcl_Interp *interp,
+			  Environment *env,
+			  int objc,
+			  Tcl_Obj *const objv[])
+{
+	Load(env, Tcl_GetString(objv[2]));
+
+	return TCL_OK;
+}
+
+static int tcl_clips_LoadFacts(Tcl_Interp *interp,
+			       Environment *env,
+			       int objc,
+			       Tcl_Obj *const objv[])
+{
+	LoadFacts(env, Tcl_GetString(objv[2]));
+
+	return TCL_OK;
+}
+
+static int tcl_clips_Reset(Tcl_Interp *interp,
+			   Environment *env,
+			   int objc,
+			   Tcl_Obj *const objv[])
+{
+	Reset(env);
+
+	return TCL_OK;
+}
+
+static int tcl_clips_Run(Tcl_Interp *interp,
+			 Environment *env,
+			 int objc,
+			 Tcl_Obj *const objv[])
+{
+	int limit;
+	Tcl_GetIntFromObj(interp, objv[2], &limit);
+	Run(env, limit);
+
+	return TCL_OK;
+}
 
 static int clips_Tcl_ObjCmdProc(ClientData clientData,
 				Tcl_Interp *interp,
@@ -20,26 +130,23 @@ static int clips_Tcl_ObjCmdProc(ClientData clientData,
 		return TCL_ERROR;
 	}
 
-	char *command = Tcl_GetString(objv[1]);
+	const char *command = Tcl_GetString(objv[1]);
 
 	switch (command[0]) {
 	case 'a':
 		assert(strcmp(command, "assert-string") == 0);
 		assert(objc == 3);
-		AssertString(env, Tcl_GetString(objv[2]));
-		break;
+		return tcl_clips_AssertString(interp, env, objc, objv);
 	case 'b':
 		switch (command[1]) {
 		case 'a':
 			assert(strcmp(command, "batch-star") == 0);
 			assert(objc == 3);
-			BatchStar(env, Tcl_GetString(objv[2]));
-			break;
+			return tcl_clips_BatchStar(interp, env, objc, objv);
 		case 'u':
 			assert(strcmp(command, "build") == 0);
 			assert(objc == 3);
-			Build(env, Tcl_GetString(objv[2]));
-			break;
+			return tcl_clips_Build(interp, env, objc, objv);
 		default:
 			assert(false);
 		}
@@ -47,45 +154,25 @@ static int clips_Tcl_ObjCmdProc(ClientData clientData,
 	case 'd':
 		assert(strcmp(command, "defglobal-get-value") == 0);
 		assert(objc == 3);
-
-		CLIPSValue v;
-		DefglobalGetValue(
-			*(void **) Tcl_GetByteArrayFromObj(objv[2], NULL),
-			&v);
-		printf("%lld\n", v.integerValue->contents);
-		Tcl_SetObjResult(interp,
-				 Tcl_NewIntObj(
-					 v.integerValue->contents));
-		break;
+		return tcl_clips_DefglobalGetValue(interp, env, objc, objv);
 	case 'e':
 		assert(strcmp(command, "eval") == 0);
 		assert(objc == 3);
-		Eval(env, Tcl_GetString(objv[2]), NULL);
-		break;
+		return tcl_clips_Eval(interp, env, objc, objv);
 	case 'f':
 		assert(strcmp(command, "find-defglobal") == 0);
 		assert(objc == 3);
-		Defglobal *r = FindDefglobal(env, Tcl_GetString(objv[2]));
-		printf("##2 %p\n", r);
-		if (r == NULL) {
-		} else {
-			Tcl_SetObjResult(interp,
-					 Tcl_NewByteArrayObj(
-						 (void *) &r, sizeof(r)));
-		}
-		break;
+		return tcl_clips_FindDefglobal(interp, env, objc, objv);
 	case 'l':
 		switch (command[4]) {
 		case 0:
 			assert(strcmp(command, "load") == 0);
 			assert(objc == 3);
-			Load(env, Tcl_GetString(objv[2]));
-			break;
+			return tcl_clips_Load(interp, env, objc, objv);
 		case '-':
 			assert(strcmp(command, "load-facts") == 0);
 			assert(objc == 3);
-			LoadFacts(env, Tcl_GetString(objv[2]));
-			break;
+			return tcl_clips_LoadFacts(interp, env, objc, objv);
 		default:
 			assert(false);
 		}
@@ -95,15 +182,11 @@ static int clips_Tcl_ObjCmdProc(ClientData clientData,
 		case 'e':
 			assert(strcmp(command, "reset") == 0);
 			assert(objc == 2);
-			Reset(env);
-			break;
+			return tcl_clips_Reset(interp, env, objc, objv);
 		case 'u':
 			assert(strcmp(command, "run") == 0);
 			assert(objc == 3);
-			int i;
-			Tcl_GetIntFromObj(interp, objv[2], &i);
-			Run(env, i);
-			break;
+			return tcl_clips_Run(interp, env, objc, objv);
 		default:
 			assert(false);
 		}
@@ -112,7 +195,7 @@ static int clips_Tcl_ObjCmdProc(ClientData clientData,
 		assert(false);
 	}
 
-	return TCL_OK;
+	return TCL_ERROR;
 }
 
 void CLIPS_Tcl_InitializeCLIPSInterface(Environment *env, Tcl_Interp *interp)
